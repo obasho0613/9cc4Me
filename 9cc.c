860563d55cebd578dd2e2e688bc25ea3cde9bbda
sobata <obasho0613@gmail.com>
2025-02-05 02:39:45 +0900

    優先度付き四則演算を抽象構文木にしてからスタックマシンで計算

--- 9cc.c
+++ 9cc.c
@@ -97,7 +97,7 @@ Token *tokenize() {
 			continue;
 		}
 		// 記号
-		if (*p == '+' || *p == '-') {
+		if (strchr("+-*/()", *p)) {
 			cur = new_token(TK_RESERVED, cur, p++);
 			continue;
 		}
@@ -116,35 +116,154 @@ Token *tokenize() {
 	return head.next;
 }
 
-int main(int argc, char **argv) {
-	if (argc != 2) {
-		fprintf(stderr, "引数の個数が正しくありません\n");
-		return 1;
+/*
+加減算の生成規則
+expr = num ("+" num | "-" num)*
+
+優先順位を含む四則演算の生成規則
+乗除算の生成規則mulを経由してnumが展開される
+expr = mul ("+" mul | "-" mul)*
+mul  = num ("*" num | "/" num)*
+
+優先順位のカッコ()を四則演算に追加した文法の生成規則
+expr    = mul ("+" mul | "-" mul)*
+mul     = primary ("*" primary | "/" primary)*
+primary = num | "(" expr ")"
+*/
+
+// 抽象構文木のノードの種類
+typedef enum {
+	ND_ADD, // +
+	ND_SUB, // -
+	ND_MUL, // *
+	ND_DIV, // /
+	ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+// 抽象構文木のノードの型
+struct Node {
+	NodeKind kind; // ノードの型
+	Node *lhs;     // 左辺(left-hand side)
+	Node *rhs;     // 右辺(right-hand side)
+	int val;       // kindがND_NUMの場合のみ扱う
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = kind;
+	node->lhs  = lhs;
+	node->rhs  = rhs;
+	return node;
+}
+
+Node *new_node_num(int val) {
+	Node *node = calloc(1, sizeof(Node));
+	node->kind = ND_NUM;
+	node->val  = val;
+	return node;
+}
+
+// コンパイルエラー回避のためのプロトタイプ宣言
+Node *expr();
+Node *mul();
+Node *primary();
+
+// expr = mul ("+" mul | "-" mul)*
+Node *expr() {
+	//printf("%c:%s\n", *(token->str), __func__);
+	Node *node = mul();
+
+	for (;;) {
+		if (consume('+'))
+			node = new_node(ND_ADD, node, mul());
+		else if (consume('-'))
+			node = new_node(ND_SUB, node, mul());
+		else
+			return node;
+	}
+}
+// mul = primary ("*" primary | "/" primary)*
+Node *mul() {
+	//printf("%c:%s\n", *(token->str), __func__);
+	Node *node = primary();
+
+	for (;;) {
+		if (consume('*'))
+			node = new_node(ND_MUL, node, primary());
+		else if (consume('/'))
+			node = new_node(ND_DIV, node, primary());
+		else
+			return node;
+	}
+}
+// primary = "(" expr ")" | num
+Node *primary() {
+	//printf("%c:%s\n", *(token->str), __func__);
+
+	// 次のトークンが"("なら、"(" expr ")"のはず
+	if (consume('(')) {
+		Node *node = expr();
+		expect(')');
+		return node;
 	}
 
-	// トークナイズする
+	// そうでなければ数値のはず
+	return new_node_num(expect_number());
+}
+
+void gen(Node *node) {
+	if (node->kind == ND_NUM) {
+		//printf("%d:%s\n", node->val, __func__);
+		printf("  push %d\n", node->val);
+		return;
+	}
+
+	gen(node->lhs);
+	gen(node->rhs);
+
+	printf("  pop rdi\n");
+	printf("  pop rax\n");
+
+	switch (node->kind) {
+	case ND_ADD:
+		printf("  add rax, rdi\n");
+		break;
+	case ND_SUB:
+		printf("  sub rax, rdi\n");
+		break;
+	case ND_MUL:
+		printf("  imul rax, rdi\n");
+		break;
+	case ND_DIV:
+		printf("  cqo\n");
+		printf("  idiv rdi\n");
+		break;
+	}
+
+	printf("  push rax\n");
+}
+
+int main(int argc, char **argv) {
+	if (argc != 2)
+		fprintf(stderr, "引数の個数が正しくありません\n");
+
+	// トークナイズしてパースする
 	user_input = argv[1];
 	token = tokenize();
+	Node *node = expr();
 
 	// アセンブリの前半部分を出力
 	printf(".intel_syntax noprefix\n");
 	printf(".global main\n");
 	printf("main:\n");
 
-	// 式の最初は数でなければならないので、それをチェックして最初のmov命令を出力
-	printf("  mov rax, %d\n", expect_number());
+	// 抽象構文木(AST)をトラバースしてアセンブリを生成する
+	gen(node);
 
-	// `+ <数>`あるいは`- <数>`というトークンの並びを消費しつつアセンブリを出力
-	while (!at_eof()) {
-		if (consume('+')) {
-			printf("  add rax, %d\n", expect_number());
-			continue;
-		}
-
-		expect('-');
-		printf("  sub rax, %d\n", expect_number());
-	}
-
+	// 結果はスタックの一番上に格納されているので、
+	// RAXにpopして、プログラムの終了コードにする。
+	printf("  pop rax\n");
 	printf("  ret\n");
 	return 0;
 }
--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ assert() {
 	input="$2"
 
 	./9cc "$input" > tmp.s
-	gcc -static -o tmp tmp.s
+	gcc -z noexecstack -static -o tmp tmp.s
 	./tmp
 	actual="$?"
 
@@ -21,6 +21,10 @@ assert 0 0
 assert 42 42
 assert 21 "5+20-4"
 assert 41 " 12 + 34 - 5 "
+assert 47 "5+6*7"
+assert 47 "5+(6*7)"
+assert 15 "5*(9-6)"
+assert 4 "(3+5)/2"
 
 echo OK
 
